ab24542d89572ecf465ec98a1c3374385400ac10
John Emerson <john.carv.sousa@gmail.com>
2021-02-22 13:25:11 -0300

    feat: add colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
-import { Button } from './components/Button'
-
 import './styles/global.css'
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <Button color="red">
-        botao vermelho
-      </Button>
-      <Button color="blue">
-        botao azul
-      </Button>
     </div>
   );
 }
